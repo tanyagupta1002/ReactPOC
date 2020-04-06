43559e581115fb8fc0411f05ce3b55ce1f5baf93
tanya_gupta <tanyagupta@globallogic.com>
2020-04-06 18:20:53 +0530

    reactpoc

---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,17 @@
+import React,{ Component} from 'react';
+import Auxillary from '../../hoc/Auxillary';
+import Burger from '../../components/Burger/Burger';
+
+class BurgerBuilder extends Component {
+    render()
+    {
+        return (
+            <Auxillary>
+                <Burger />
+                <div>Build Controls</div>
+            </Auxillary>
+        )
+    }
+}
+
+export default BurgerBuilder;
